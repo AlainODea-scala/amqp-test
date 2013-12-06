5ce69e5948a235627abdeaa934c7fdc986cb88d8
Alain O'Dea <alain.odea@verafin.com>
2013-12-06 22:03:18 +0000

    Clarify error reporting

--- README.md
+++ README.md
@@ -6,3 +6,35 @@ Usage:
 ------
 * Run sbt assembly
 * Run java -jar amqp-test-assembly-1.0.jar
+* Exit code 0 means success
+* Exit code 1 means failure (details will be printed to STDERR)
+
+Connection.properties
+---------------------
+* hostName: String - host name of RabbitMQ server
+* tls: Boolean - whether or not to use TLS encrypted communications (default true)
+* port: Int - port number to connect to (default when tls 5671, otherwise 5672)
+* commandQueue: String - name of the command queue to declare
+* responseQueue: String - name of the response queue to declare
+* username: String - user name to authenticate with
+* password: String - password for that user
+* virtualHost: String - vHost to connect to (default /)
+
+**Example basic connection.properties (TLS defaults to true)**:
+
+    hostName=localhost
+    tls=false
+    queue=queueName
+    username=username
+    password=somesensiblepassword
+    virtualHost=vhostname
+
+**Example: more involved connection.properties**:
+
+    hostName=localhost
+    port=443
+    commandQueue=command
+    responseQueue=response
+    username=username
+    password=somesensiblepassword
+    virtualHost=vhostname
--- src/main/scala/com/alainodea/AMQPTestMain.scala
+++ src/main/scala/com/alainodea/AMQPTestMain.scala
@@ -1,10 +1,10 @@
 package com.alainodea
 
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.{AlreadyClosedException, ConnectionFactory}
 import java.util.Properties
 import collection.JavaConverters._
 import com.github.nscala_time.time.Imports._
-import java.io.FileReader
+import java.io.{EOFException, FileReader}
 
 object AMQPTestMain {
   def main(args: Array[String]) {
@@ -12,32 +12,77 @@ object AMQPTestMain {
     val configFile = if (args.length > 0) args(0) else "connection.properties"
     properties.load(new FileReader(configFile))
     val config = new Config(properties.asScala, configFile)
-    val hostName = config("hostName")
-    val port = config("port").toInt
-    val commandQueue = config("commandQueue")
-    val responseQueue = config("responseQueue")
-    val username = config("username")
-    val password = config("password")
-    val virtualHost = config("virtualHost")
+
+    val hostName =      config("hostName"                                  )
+    val tls =           config("tls"          , "true"                     ).toBoolean
+    val port =          config("port"         , if (tls) "5671" else "5672").toInt
+    val commandQueue  = config("commandQueue"                              )
+    val responseQueue = config("responseQueue"                             )
+    val username =      config("username"                                  )
+    val password =      config("password"                                  )
+    val virtualHost =   config("virtualHost"  , "/"                        )
+
     val factory = new ConnectionFactory
-    factory.useSslProtocol()
+    if (tls) factory.useSslProtocol()
     factory.setHost(hostName)
     factory.setVirtualHost(virtualHost)
     factory.setPort(port)
     factory.setUsername(username)
     factory.setPassword(password)
     factory.setConnectionTimeout(5.seconds.millis.toInt)
-    val connection = factory.newConnection()
-    val channel = connection.createChannel()
-    channel.queueDeclare(commandQueue, true, false, false, null)
-    channel.queueDeclare(responseQueue, true, false, false, null)
-    channel.close()
-    connection.close()
 
-    println("All good")
+    intercept {
+      bracket(factory.newConnection())(_.close()) { connection =>
+        bracket(connection.createChannel())(_.close()) { channel =>
+          channel.queueDeclare(commandQueue, true, false, false, null)
+          channel.queueDeclare(responseQueue, true, false, false, null)
+          channel.close()
+        }
+      }
+    } match {
+      case None =>
+        sys.exit(0)
+      case Some(cause) =>
+        cause match {
+          case eof: EOFException =>
+            Console.err.println(eof + " - this is a protocol error. Sometimes caused by non-existent virtualHost.")
+          case e =>
+            Console.err.println(e)
+        }
+        sys.exit(1)
+    }
   }
 
   class Config(config: collection.mutable.Map[String,String], configResource: String) {
-    def apply(key: String): String = config.getOrElse(key, sys.error("missing '" + key + "' from " + configResource))
+    def apply(key: String): String = apply(key, sys.error("missing '" + key + "' from " + configResource))
+    def apply(key: String, default: => String): String = config.getOrElse(key, default)
+  }
+
+  def bracket[T](alloc: => T)(release: T => Unit)(block: T => Unit) {
+    val it = alloc
+    try {
+      block(it)
+    } finally {
+      try {
+        release(it)
+      } catch {
+        case e: AlreadyClosedException =>
+      }
+    }
+  }
+
+  def intercept(block: => Unit): Option[Throwable] = {
+    try {
+      block
+      None
+    } catch {
+      case e: Throwable =>
+        var cause = e.getCause
+        if (cause == null) cause = e
+        else {
+          while (cause.getCause != null) cause = cause.getCause
+        }
+        Some(cause)
+    }
   }
 }
